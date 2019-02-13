97a83a9d0f376e452e6610400df9e9ba2d885882
Matt <mpau548@aucklanduni.ac.nz>
2019-02-13 16:44:50 +1300

    We now populate the proposal list with proposals. Basic styling has been added

--- src/app/community-fund/proposal-list/proposal-list.component.ts
+++ src/app/community-fund/proposal-list/proposal-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WalletService } from 'src/app/wallet/wallet.service';
+import { RpcSend } from 'src/app/rpc/rpc-send.model';
+import { RpcReceive } from 'src/app/rpc/rpc-receive.model';
 
 @Component({
   selector: 'app-proposal-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProposalListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private walletService: WalletService) { }
+  proposalList: Array<CFProposal>;
 
   ngOnInit() {
+    this.fetchProposals();
   }
 
+  fetchProposals() {
+    const rpcData = new RpcSend('listproposals');
+    this.walletService.sendRPC(rpcData)
+      .subscribe(
+        (receive: RpcReceive) => {
+          if (receive.type === 'SUCCESS') {
+            console.log(`Proposal Data: ${receive.data[0]}`);
+            this.proposalList = [...receive.data];
+            console.table(this.proposalList);
+          } else {
+            console.log('error: ', receive);
+          }
+        }, error => {
+          console.log('error: ', error);
+        }
+      );
+
+  }
 }

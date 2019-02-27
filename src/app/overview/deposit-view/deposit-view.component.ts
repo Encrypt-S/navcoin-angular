a9de51a75437d9777f44781024de225438592489
djksa <nopumper@gmail.com>
2019-02-28 10:20:18 +1300

    added generate new address feature

--- src/app/overview/deposit-view/deposit-view.component.ts
+++ src/app/overview/deposit-view/deposit-view.component.ts
@@ -3,16 +3,17 @@ import { ExplorerService } from '../../explorer/explorer.service';
 import { WalletModel } from '../../wallet/wallet.model';
 import { WalletService } from '../../wallet/wallet.service';
 import { RpcReceive } from '../../rpc/rpc-receive.model';
-
+import { RpcSend } from 'src/app/rpc/rpc-send.model';
 
 @Component({
   selector: 'app-deposit-view',
   templateUrl: './deposit-view.component.html',
-  styleUrls: ['../overview.component.css','./deposit-view.component.css']
+  styleUrls: ['../overview.component.css', './deposit-view.component.css']
 })
 export class DepositViewComponent implements OnInit {
   wallet: WalletModel;
   rpcReceive: RpcReceive;
+
   qrMainAddress: string;
 
   constructor(
@@ -21,10 +22,35 @@ export class DepositViewComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.wallet = {
-      ...this.wallet,
-      mainAddress: 'NaSdzJ64o8DQo5DMPexVrL4PYFCBZqcmsW'
-    };
-    this.qrMainAddress = `navcoin:${this.wallet.mainAddress}?label=NavPi`;
+    this.getMainAddress();
+  }
+
+  getMainAddress() {
+    console.log('getaddress called');
+    this.walletService.sendAPI('getmainaddress').subscribe(
+      (receieve: RpcReceive) => {
+        const address = receieve.data.address;
+        this.qrMainAddress = `navcoin:${address}?label=NavPi`;
+      },
+      error => {
+        // this.notificationService.addNotification(
+        //   new Notification(
+        //     `Unable to get transactions ${error}`,
+        //     NotifType.ERROR
+        //   )
+        // );
+        return;
+      }
+    );
+  }
+
+  getNewAddress() {
+    console.log('getnewaddress called');
+    const data = new RpcSend('getnewaddress');
+    this.walletService.sendRPC(data).subscribe((receive: RpcReceive) => {
+      const address = receive.data;
+      console.log(address);
+      this.qrMainAddress = `navcoin${address}?label=NavPi`;
+    });
   }
 }

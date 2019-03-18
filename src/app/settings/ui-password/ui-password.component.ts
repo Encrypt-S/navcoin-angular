5b79b9ec3b56188bef048f08015b1573e569fd9b
Matt <mpau548@aucklanduni.ac.nz>
2019-03-19 12:01:31 +1300

    Added confirm field to web interface login details update

--- src/app/settings/ui-password/ui-password.component.ts
+++ src/app/settings/ui-password/ui-password.component.ts
@@ -10,44 +10,57 @@ import { MzToastService } from 'ngx-materialize';
   templateUrl: './ui-password.component.html'
 })
 export class UiPasswordComponent implements OnInit {
-
   uiPassword = new UiPasswordModel();
   uiPasswordResponse: UiPasswordResponse;
 
   constructor(
     private uiPasswordService: UiPasswordService,
-    private toastService: MzToastService,
+    private toastService: MzToastService
   ) {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   onSubmit() {
-    console.log('ui password: ', this.uiPassword);
-
-    this.uiPasswordService.update(this.uiPassword)
-      .subscribe(
+    if (this.uiPassword.password !== this.uiPassword.confirmPassword) {
+      this.toastService.show(
+        'Your new password do not match the confirm field, please check you typed it correctly in both fields',
+        5000,
+        'red'
+      );
+      return;
+    } else {
+      this.uiPasswordService.update(this.uiPassword).subscribe(
         (response: UiPasswordResponse) => {
-
           if (response.type != 'SUCCESS') {
             if (response.code == 'AUTH_002') {
-              this.toastService.show('The current username or password you entered is invalid', 4000, 'red');
-              return
+              this.toastService.show(
+                'The current username or password you entered is invalid',
+                4000,
+                'red'
+              );
+              return;
             }
-            this.toastService.show('Something went wrong, try again', 4000, 'red');
-            return
+            this.toastService.show(
+              'Something went wrong, try again',
+              4000,
+              'red'
+            );
+            return;
           }
 
           this.uiPassword = new UiPasswordModel();
           this.toastService.show('User Login Updated', 4000, 'green');
-          return
-
-        }, error => {
+          return;
+        },
+        error => {
           console.log('error: ', error);
-          this.toastService.show('Something went wrong, try again', 4000, 'red');
+          this.toastService.show(
+            'Something went wrong, try again',
+            4000,
+            'red'
+          );
         }
       );
+    }
   }
-
 }

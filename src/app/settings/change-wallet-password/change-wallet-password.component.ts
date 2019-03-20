8393584934a15a6e3c085b59b9d08a4ad6714747
Matt <mpau548@aucklanduni.ac.nz>
2019-03-20 17:22:40 +1300

    Implemented change wallet password

--- src/app/settings/change-wallet-password/change-wallet-password.component.ts
+++ src/app/settings/change-wallet-password/change-wallet-password.component.ts
@@ -4,12 +4,14 @@ import { MzToastService } from 'ngx-materialize';
 import { WalletService } from 'src/app/wallet/wallet.service';
 import { RpcReceive } from 'src/app/rpc/rpc-receive.model';
 import { FormControl, FormGroup } from '@angular/forms';
+import { RpcSend } from 'src/app/rpc/rpc-send.model';
 
 @Component({
   selector: 'app-change-wallet-password',
   templateUrl: './change-wallet-password.component.html'
 })
 export class ChangeWalletPasswordComponent implements OnInit {
+  buttonDebounce = false;
   passwordForm = new FormGroup({
     currentPass: new FormControl(''),
     newPass: new FormControl(''),
@@ -23,5 +25,57 @@ export class ChangeWalletPasswordComponent implements OnInit {
 
   ngOnInit() {}
 
-  onSubmit() {}
+  onSubmit() {
+    if (
+      this.passwordForm.value.newPass !== this.passwordForm.value.confirmNewPass
+    ) {
+      this.toastService.show(
+        'New wallet password does not match the confirm field.',
+        5000,
+        'red'
+      );
+      this.buttonDebounce = false;
+      return;
+    }
+    this.buttonDebounce = true;
+    this.toastService.show(
+      'Wallet password is updating, please wait',
+      10000,
+      'green'
+    );
+    this.walletService
+      .sendRPC(
+        new RpcSend('walletpassphrasechange', [
+          this.passwordForm.value.currentPass,
+          this.passwordForm.value.newPass
+        ])
+      )
+      .subscribe(
+        (receive: RpcReceive) => {
+          if (receive.type === 'SUCCESS') {
+            this.toastService.show(
+              'Wallet password successfully updated, please restart your Stakebox to begin using your encrypted wallet.',
+              10000,
+              'green'
+            );
+            this.passwordForm.reset();
+          } else {
+            this.toastService.show(
+              'Failed to update wallet password.',
+              5000,
+              'red'
+            );
+          }
+          this.buttonDebounce = false;
+        },
+        error => {
+          this.toastService.show(
+            'Failed to update wallet password.',
+            5000,
+            'red'
+          );
+          this.buttonDebounce = false;
+        }
+      );
+  }
 }
